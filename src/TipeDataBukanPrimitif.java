3d4ba59cf50273cd83e98f9502fe7b546ea1ad3b
onowdev <onowdev@gmail.com>
2021-11-27 11:35:46 +0700

    add Variabel dan tipedata Bukan Primitif

---
+++ src/TipeDataBukanPrimitif.java
@@ -0,0 +1,13 @@
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+        Integer iniInteger = 900;
+        Long iniLong = 90000L;
+
+        Byte iniByte = null;
+
+        System.out.println(iniByte);
+        iniByte = 100;
+
+        System.out.println(iniByte);
+    }
+}
---
+++ src/TipeVariabel.java
@@ -0,0 +1,23 @@
+public class TipeVariabel {
+    public static void main(String[] args) {
+        String fullname;
+        fullname = "Agus Sugiono";
+
+        System.out.println(fullname);
+
+        int age = 30;
+        String alamat= "Desa Di Magelang";
+
+        fullname = "Andra Sugiono";
+
+        System.out.println(age);
+        System.out.println(alamat);
+        System.out.println(fullname);
+
+        //Menggunakan Kata Kunci Var
+
+        var nama1 = "Agus Sugiono";
+        var umur = 31;
+
+    }
+}
