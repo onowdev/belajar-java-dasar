7c950095a531e9de7573c7e8e854bfd9328c29f4
onowdev <onowdev@gmail.com>
2021-11-27 10:56:10 +0700

    add Tipe data bollean dan String

---
+++ src/TipeDataString.java
@@ -0,0 +1,9 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Agus";
+        String lastName = "Sugiono";
+
+        System.out.print(firstName);
+        System.out.println(lastName);
+    }
+}
