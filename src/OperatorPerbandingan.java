3f6e0606eb79c774fcbc99a060df62216bcb6261
onowdev <onowdev@gmail.com>
2021-12-02 10:28:52 +0700

    Operator Perbandingan 2 nilai

---
+++ src/OperatorPerbandingan.java
@@ -0,0 +1,20 @@
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        /*
+        Operasi perbandingan adalah operasi untuk membandingkan dua buah data
+        Operasi perbandingan adalah operasi yang menghasilkan nilai boolean (benar atau salah)
+        Jika hasil operasinya adalah benar, maka nilainya adalah true
+        Jika hasil operasinya adalah salah, maka nilainya adalah false
+        */
+        int value1 = 100;
+        int value2 = 200;
+
+        System.out.println(value1 > value2);
+        System.out.println(value1 < value2);
+        System.out.println(value1 >= value2);
+        System.out.println(value1 <= value2);
+        System.out.println(value1 == value2);
+        System.out.println(value1 != value2);
+
+    }
+}
