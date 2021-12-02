7fa331c60bc77364274b66890402fc3d767ec71d
onowdev <onowdev@gmail.com>
2021-12-02 10:03:52 +0700

    Menulis Array di Java

--- src/TipeDataArray.java
+++ src/TipeDataArray.java
@@ -9,9 +9,36 @@ public class TipeDataArray {
         arrayString[2] = "Andra";
         arrayString[3] = "Sugiono";
 
+    //Array Initializer
+
+        int[] arrayInt = new int[]{
+          10, 90, 80, 99, 900
+        };
+    // Array Long
+        long[] arrayLong = {
+                10,90,90000,9,9999
+        };
+    // Array di dalam array
+        String[][] members ={
+                {"Agus", "Sugiono", "Pranoto"},
+                {"Devy", "Ramadhani"},
+                {"Andra"}
+        };
+        // Akses Array dalam array
+        String[] member1 = members[0];
+        System.out.println(member1[0]);
+
+        System.out.println(members[1][0]);
+        System.out.println(members[2][0]);
+
+        System.out.println(arrayLong[4]);
+        System.out.println(arrayInt[3]);
         System.out.println(arrayString[0]);
         System.out.println(arrayString[1]);
         System.out.println(arrayString[2]);
         System.out.println(arrayString[3]);
+        // Mencari panjang array dengan Perintah Length
+
+        System.out.println(arrayLong.length);
     }
 }
