4537a1363ed031f2aab2cf3bccbbd4ea6146dd68
onowdev <onowdev@gmail.com>
2021-12-02 09:08:18 +0700

    learn Array

---
+++ src/TipeDataArray.java
@@ -0,0 +1,17 @@
+public class TipeDataArray {
+    public static void main(String[] args) {
+
+        String[] arrayString;
+        arrayString = new String[4];
+
+        arrayString[0] = "Agus";
+        arrayString[1] = "Sugiono";
+        arrayString[2] = "Andra";
+        arrayString[3] = "Sugiono";
+
+        System.out.println(arrayString[0]);
+        System.out.println(arrayString[1]);
+        System.out.println(arrayString[2]);
+        System.out.println(arrayString[3]);
+    }
+}
