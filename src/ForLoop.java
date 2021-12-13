880a1b7bfd85d9912ce33fe7ce560ecf9546f832
onowdev <onowdev@gmail.com>
2021-12-13 16:15:49 +0700

    Latihan forLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        var counter = 1;
+
+        for (;counter <=10;){
+            System.out.println("Perulangan ke-" +counter);
+            counter++;
+        }
+    }
+}
