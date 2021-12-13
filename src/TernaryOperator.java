f7149fb5939f717c5342e847e90eed9901b228ad
onowdev <onowdev@gmail.com>
2021-12-13 15:45:05 +0700

    Latihan Switch case

---
+++ src/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args){
+        var nilai = 80;
+        String ucapan = nilai >=80 ? "Selamat Anda Lulus" : "Silahkan Ujian Lagi";
+
+        System.out.println(ucapan);
+    }
+}
