713ae77cd77a1393798d70f2b33ce8905762e8c3
onowdev <onowdev@gmail.com>
2021-12-13 16:30:39 +0700

    add For init&Post statement

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -6,5 +6,16 @@ public class ForLoop {
             System.out.println("Perulangan ke-" +counter);
             counter++;
         }
+        //For dengan initStatement
+
+        for (var hitung = 1; hitung <=10;){ // letakkan Var di dalam kurung for
+            System.out.println("For initstatement ke-" + hitung);
+            hitung++;
+        }
+        //For dengan PostStatement
+
+        for (var bilangan = 1; bilangan <=10; bilangan++){
+            System.out.println("ForPostStatement ke-" + bilangan);
+        }
     }
 }
