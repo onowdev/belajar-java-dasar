ee1c306775dd25d7dab23643200f58809f55244b
onowdev <onowdev@gmail.com>
2021-12-16 21:47:02 +0700

    Latihan Perulangan

---
+++ src/MethodInJava.java
@@ -0,0 +1,11 @@
+public class MethodInJava {
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+    static void sayHelloWorld(){ // Membuat method
+        System.out.println("Hello World");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+        System.out.println("Hello World 4");
+    }
+}
---
+++ src/PerulanganForEach.java
@@ -0,0 +1,11 @@
+public class PerulanganForEach {
+    public static void main(String[] args) {
+        String[] names ={
+                "Agus", "Sugiono", "Dzakiandra",
+                "Alfarizqi", "Sugiono", "Devy"
+        };
+        for (var name: names){
+            System.out.println(name);
+        }
+    }
+}
