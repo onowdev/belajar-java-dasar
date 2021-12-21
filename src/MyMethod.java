6e21e48ccddb5ac012fc38d74debe4b928b9b675
onowdev <onowdev@gmail.com>
2021-12-21 08:58:57 +0700

    Method Another way

---
+++ src/MyMethod.java
@@ -0,0 +1,9 @@
+public class MyMethod {
+    static void myMethod(){
+        System.out.println("Hello World");
+        System.out.println("Hello Agus");
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
